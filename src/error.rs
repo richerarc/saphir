fe6918577435b1362aff7e6b6fa4d1d998f818a2
Richer Archambault <richer.arc@gmail.com>
2018-06-04 12:05:26 -0400

    updated to hyper 0.12

--- src/error.rs
+++ src/error.rs
@@ -1,8 +1,12 @@
+use std::fmt;
+
 /// Potential server errors
 #[derive(Debug)]
 pub enum ServerError {
     /// An Hyper Error
     HyperError(::hyper::Error),
+    /// A cancellation Error
+    FutureCancelledError(::futures::Canceled),
     /// A parsing error of addr
     ParseError(::std::net::AddrParseError),
 }
@@ -16,5 +20,33 @@ impl From<::std::net::AddrParseError> for ServerError {
 impl From<::hyper::Error> for ServerError {
     fn from(e: ::hyper::Error) -> Self {
         ServerError::HyperError(e)
+    }
+}
+
+impl From<::futures::Canceled> for ServerError {
+    fn from(e: ::futures::Canceled) -> Self {
+        ServerError::FutureCancelledError(e)
+    }
+}
+
+impl ::std::error::Error for ServerError {
+    fn description(&self) -> &str {
+        use ServerError::*;
+        match self {
+            HyperError(ref e) => e.description(),
+            FutureCancelledError(ref e) => e.description(),
+            ParseError(ref e) => e.description(),
+        }
+    }
+}
+
+impl ::std::fmt::Display for ServerError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result<> {
+        use ServerError::*;
+        match self {
+            HyperError(ref e) => e.fmt(f),
+            FutureCancelledError(ref e) => e.fmt(f),
+            ParseError(ref e) => e.fmt(f),
+        }
     }
 }

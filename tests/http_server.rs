381107a469f910b11aa68e230a2fe070932715ae
Richer Archambault <richer.arc@gmail.com>
2018-06-07 09:29:16 -0400

    fix test

--- tests/http_server.rs
+++ tests/http_server.rs
@@ -17,30 +17,28 @@ struct TestControllerContext {
     pub resource: String,
 }
 
-impl Default for TestControllerContext {
-    fn default() -> Self {
+impl TestControllerContext {
+    pub fn new(res: &str) -> Self {
         TestControllerContext {
-            resource: "This is a string".to_string(),
+            resource: res.to_string(),
         }
+    }
+
+    pub fn function_to_receive_any_get_http_call(&self, req: &SyncRequest, res: &mut SyncResponse) {
+        use saphir::header::*;
+        res.status(StatusCode::OK).body(format!("this is working nicely!\r\n the context string is : {}", self.resource));
     }
 }
 
-fn function_to_receive_any_get_http_call(context: &TestControllerContext, req: &SyncRequest, res: &mut SyncResponse) {
-    use saphir::header::*;
-    let heads: Headers = req.parsed_header();
-    res.status(StatusCode::OK).body(format!("this is working nicely!\r\n the context string is : {}", context.resource))
-        .parsed_header(heads);
-}
-
 #[test]
-fn http_server() {
+fn simple_http_server() {
     let mut mid_stack = MiddlewareStack::new();
 
     mid_stack.apply(TestMiddleware {}, vec!("/"), None);
 
-    let basic_test_cont = BasicController::new(TestControllerContext::default());
+    let basic_test_cont = BasicController::new(TestControllerContext::new("this is a private resource"));
 
-    basic_test_cont.add(Method::GET, reg!("/"), function_to_receive_any_get_http_call);
+    basic_test_cont.add(Method::GET, reg!("/"), TestControllerContext::function_to_receive_any_get_http_call);
     basic_test_cont.add(Method::POST, reg!("/"), |_, _, _| { println!("this was a post request") });
     basic_test_cont.add_with_guards(Method::PUT, "^/patate", BodyGuard.into(), |_,_,_| {println!("this is only reachable if the request has a body")});
 

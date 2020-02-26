f250f4b3096b8a11a3958b5562f7a93e9e7f4af0
Richer Archambault <richer.arc@gmail.com>
2020-02-26 15:29:02 -0500

    Added HandlerAttrs type

--- saphir_macro/src/handler.rs
+++ saphir_macro/src/handler.rs
@@ -11,6 +11,47 @@ use syn::export::{Span, ToTokens};
 use proc_macro2::TokenStream;
 use crate::controller::ControllerAttr;
 
+pub struct HandlerAttrs {
+    method: Method,
+    path: String,
+    guards: Vec<Ident>,
+    is_async: bool,
+}
+
+impl HandlerAttrs {
+    pub fn new(mut attrs: Vec<Attribute>) -> Self {
+        let mut method = None;
+        let mut path = String::new();
+
+        let metas = attrs.iter_mut().map(|attr| attr.parse_meta().expect("Invalid function arguments")).collect::<Vec<Meta>>();
+        for meta in metas {
+            match meta {
+                Meta::List(l) => {
+                    if let Some(ident) = l.path.get_ident() {
+                        method = Some(Method::from_str(ident.to_string().to_uppercase().as_str()).expect("Invalid HTTP method"));
+                    }
+
+                    if let Some(NestedMeta::Lit(Lit::Str(str))) = l.nested.first() {
+                        path = str.value();
+                        if !path.starts_with("/") {
+                            panic!("Path must start with '/'")
+                        }
+                    }
+                }
+                Meta::NameValue(_) => { panic!("Invalid format") }
+                Meta::Path(_) => { panic!("Invalid format") }
+            }
+        }
+
+        HandlerAttrs {
+            method: method.expect("HTTP method is missing"),
+            path,
+            guards: vec![],
+            is_async: true,
+        }
+    }
+}
+
 pub fn parse_handlers(input: &ItemImpl) -> Vec<ImplItemMethod> {
     let mut vec = Vec::new();
     for item in &input.items {
@@ -22,38 +63,11 @@ pub fn parse_handlers(input: &ItemImpl) -> Vec<ImplItemMethod> {
     vec
 }
 
-pub fn parse_fn_metas(mut attrs: Vec<Attribute>) -> (Method, String) {
-    let mut method = None;
-    let mut path = String::new();
-
-    let metas = attrs.iter_mut().map(|attr| attr.parse_meta().expect("Invalid function arguments")).collect::<Vec<Meta>>();
-    for meta in metas {
-        match meta {
-            Meta::List(l) => {
-                if let Some(ident) = l.path.get_ident() {
-                    method = Some(Method::from_str(ident.to_string().to_uppercase().as_str()).expect("Invalid HTTP method"));
-                }
-
-                if let Some(NestedMeta::Lit(Lit::Str(str))) = l.nested.first() {
-                    path = str.value();
-                    if !path.starts_with("/") {
-                        panic!("Path must start with '/'")
-                    }
-                }
-            }
-            Meta::NameValue(_) => { panic!("Invalid format") }
-            Meta::Path(_) => { panic!("Invalid format") }
-        }
-    }
-
-    (method.expect("HTTP method is missing"), path)
-}
-
 pub fn gen_handlers_fn(attr: &ControllerAttr, handlers: Vec<ImplItemMethod>) -> TokenStream {
     let mut handler_stream = TokenStream::new();
     let ctrl_ident = attr.ident.clone();
     for handler in handlers {
-        let (method, path) = parse_fn_metas(handler.attrs);
+        let HandlerAttrs { method, path, guards: _, is_async: _ } = HandlerAttrs::new(handler.attrs);
         let method = Ident::new(method.as_str(), Span::call_site());
         let handler_ident = handler.sig.ident;
 

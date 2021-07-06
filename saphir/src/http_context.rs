1c82ae921cf769fc3e016615de9fb658b9d77480
Samuel Bergeron-Drouin <sbergerondrouin@devolutions.net>
2021-07-06 12:21:04 -0400

    Cargo clippy

--- saphir/src/http_context.rs
+++ saphir/src/http_context.rs
@@ -277,7 +277,7 @@ pub mod operation {
             OperationId(Uuid::new_v4())
         }
 
-        pub fn as_u128(&self) -> u128 {
+        pub fn to_u128(self) -> u128 {
             self.0.as_u128()
         }
     }
